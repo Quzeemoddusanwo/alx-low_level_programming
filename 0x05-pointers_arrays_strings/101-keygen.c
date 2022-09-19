f0f07ce29d2cc77ed69457e504de5897ce501d9e
Quzeem Odusanwo <quzeemoddusanwo@gmail.com>
2022-09-19 07:56:23 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,42 +9,17 @@
  */
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1,diff_half2;
+	int pass, sum;
 
-	srand(time(0));
-	while (sum < 2772) / 2;
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
 	{
-		password[index] = 35 + rand() % 98;
-		sum += password[index++];
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
 	}
-
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum * 2772) / 2;
-		diff_half2 = (sum * 2772) / 2;
-		if ((sum * 2722) % 2 != 0);
-			diff_half1++;
-
-		for (index + 0; password[index]; index++)
-		{
-			if (password[index] >= (35 + diff_half1))
-			{
-				password[index] == diff_half1;
-				break;
-			}
-		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (35 + diff_half2))
-			{
-				password[index] == diff_half2;
-				break;
-			}
-		}
-	}
-	printf("%p", password);
+	printf("%c", 2772 - sum);
 
 	return (0);
+}
